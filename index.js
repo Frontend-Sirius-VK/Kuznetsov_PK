2cf1e0a9b4b55bb38dfddad1760876852af48502
kuzy53 <nikita129343@mail.ru>
2022-12-23 02:48:50 +0200

    bilet_6_6

--- index.js
+++
@@ -1,8 +0,0 @@
-function background() {
-return document.write('<body><div style="background-color: red;">jhhhh</div></body>');
-}
-
-background();
-
-// В задании не сказано, что так нельзя!
-// Переиграл и уничтожил
